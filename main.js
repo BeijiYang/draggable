c29b6e33f71401d7535b46ecec468ee1a49a3301
BeijiYang <langyuyang@outlook.com>
2020-09-09 18:25:20 +0200

    support to drag in the normal flow

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ draggable.addEventListener('mousedown', evt => {
   const drag = ({ clientX, clientY }) => {
     movedX = clientX - startX;
     movedY = clientY - startY
-    draggable.style.transform = `translate(${baseX + movedX}px, ${baseY + movedY}px)`;
+    // draggable.style.transform = `translate(${baseX + movedX}px, ${baseY + movedY}px)`;
+    const range = getNearest(clientX, clientY);
+    range.insertNode(draggable); // insert 方法都会把插入的元素从原位置上移除
   }
   const mouseup = () => {
     baseX += movedX;
@@ -22,3 +24,36 @@ draggable.addEventListener('mousedown', evt => {
   document.addEventListener('mousemove', drag);
   document.addEventListener('mouseup', mouseup);
 })
+
+document.addEventListener('selectstart', evt => evt.preventDefault())
+
+const ranges = [];
+
+const container = document.getElementById('container');
+const textNode = container.childNodes[0];
+
+
+for (let i = 0; i < textNode.textContent.length; i++) {
+  const range = document.createRange();
+  range.setStart(textNode, i);
+  range.setEnd(textNode, i);
+  // console.log(range.getBoundingClientRect())
+  ranges.push(range);
+}
+// console.log(container.childNodes[0].textContent)
+
+function getNearest(x, y) {
+  let min = Infinity;
+  let nearest = null;
+
+  for (const range of ranges) {
+    const rect = range.getBoundingClientRect();
+    const distance = (rect.x - x) ** 2 + (rect.y - y) ** 2;
+
+    if (distance < min) {
+      nearest = range;
+      min = distance;
+    }
+  }
+  return nearest;
+}
